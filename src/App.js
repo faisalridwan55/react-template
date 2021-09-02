5d37925392607d746d1b807dec8c993d2cc2f550
Faisal Ridwan <faisalridwan55@gmail.com>
2021-09-02 08:45:45 +0700

    install react router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          This is basic template, change branch if you wanna find another templates
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{display: "flex", justifyContent:"center", alignContent: "center"}}>
+      This is just blank page with react-router installed
     </div>
   );
 }
